5851829d1a00a2404bf21dbf88d669925c2368aa
Sudarsan Mansingh <108286640+sudarsan2k5@users.noreply.github.com>
2022-07-07 06:11:04 +0000

    Addition two Number

---
+++ .ccls-cache/@home@runner@C-Learning/Getting Started@add_two_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a = 300;
+    int b = 654;
+    int sum = a + b;
+
+    printf("First Number = %d\n", a);
+    printf("Second Number = %d\n", b);
+    printf("%d + %d = %d\n", a, b, sum);
+    
+}
